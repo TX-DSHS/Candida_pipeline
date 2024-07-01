6a3f90a60e7d652e953c1cc2b9a77b8414fe1518
jessres <jessica.respress@dshs.texas.gov>
2024-07-01 21:13:28 +0000

    bug fix in out dir

--- mycosnptx.sh
+++ mycosnptx.sh
@@ -11,7 +11,7 @@ run_dir=/home/dnalab/Candida_auris/mycosnp-nf
 samplesheet_dir=${PWD}/mycosnp-nf/samplesheet
 samplesheet=${run_name}.csv
 prefix=/bioinformatics/Candida_auris/mycosnp-nf/output/${run_name}
-out=/bioinformatics/Candida_auris/mycosnp-nf/output/
+out=/bioinformatics/Candida_auris/mycosnp-nf/output
 sra_file=${samplesheet_dir}
 
 
@@ -21,7 +21,7 @@ sra_file=${samplesheet_dir}
 #mkdir $run_dir/reads/zip
 rm ${work_dir}/.nextflow.* #remove old nextflow.logs (06/25/24)
 rm -r ${run_dir}/reads/CA* #remove old reads (06/25/24)
-rm -r ${out}/* #remove old run (06/25/24)
+rm -r ${out}/CA* #remove old run (06/25/24)
 mkdir ${run_dir}/reads/${run_name}
 mkdir ${run_dir}/samplesheet
 mkdir ${prefix}
